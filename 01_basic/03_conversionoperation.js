2c299938bd8852a9abe7250f31b2bd8e1ba96ba1
Awnish <dubeyawnish203@gmail.com>
2026-08-24 21:14:40 +0000

    conversion of number

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -1,4 +1,30 @@
-let score = 34  
+let score = "34xyz" 
 
 console.log(typeof score);
 console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber)
+
+//33 => 33
+//34xyz => NaN (Not a Number)
+// true => 1 : False => 0
+
+
+let IsLoggedIn = "1"
+let booleanValue = Boolean(IsLoggedIn)
+console.log(booleanValue);
+
+// 1 => true
+// 0 => false
+// "" => false
+// null => false
+// undefined => false
+// NaN => false
+// "string" => true
+
+let numbers= 33.5
+let conversion= String(numbers);
+console.log(numbers)
+console.log(typeof(conversion))
