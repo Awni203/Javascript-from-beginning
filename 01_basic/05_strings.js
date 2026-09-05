57132ad6f01845a48b4f43c47992dbc0a941e179
Awnish <dubeyawnish203@gmail.com>
2026-09-05 20:58:28 +0000

    String file created

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,10 @@
+const name = 'Awnish'
+const repocount = 100
+
+// console.log(name + repocount + " Value") (OLD METHOD TO CONCATENATE STRINGS AND VARIABLES OR ANY OTHER DATA TYPE)
+
+/*new method*/console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repocount}`); // $ is used to access the variable or any other data type inside the string. This is called template literals or template strings. It is a new feature in ES6. It is used to create multi-line strings and to interpolate variables and expressions into strings.
+
+const gameName = String('Awnish Dubey'); // new method to declare a string Variable.
+console.log(gameName);
+
