d6675060060560dbcb8a761477102f95c327b8b7
Awnish <dubeyawnish203@gmail.com>
2026-09-15 12:02:40 +0000

    Add number and math operations examples

---
+++ 01_basic/06_nums&maths.js
@@ -0,0 +1,32 @@
+const score =  400
+// console.log(score)
+
+const balance = new Number(1000.5249)
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+
+const otherNumber= 1000.5249
+// console.log(otherNumber.toPrecision(4)) 
+// It will round the number to 4 significant digits
+
+const hundered = 1000000
+// console.log(hundered.toLocaleString('en-IN'))
+ // It will format the number with commas for thousands .toLocaleString('en-IN') It will format the number with commas for thousands in Indian format
+
+
+ //*****************************************Maths*******************************************
+
+ console.log(Math)
+ console.log(Math.abs(-10)) // It will return the absolute value of a number only negative will be converted into positive, positive value will remain positive.
+
+ console.log(Math.round(10.4)) // It will round the number to the nearest integer.
+ console.log(Math.ceil(10.5)) // It will round the number to the next integer.
+ console.log(Math.floor(10.5)) // It will round the number to the previous integer.
+ console.log(Math.sqrt(25)) // It will return the square root of a number.
+ console.log(Math.pow(5, 2)) // It will return the value of a number raised to the power of another number. (.pow(base, exponent))
+ console.log(Math.min(10,5,6,8,9,100,200,500,81,42)) // It will return the minimum value from a list or array of numbers.
+ console.log(Math.max(10,5,6,8,9,100,200,500,81,42)) // It will return the maximum value from a list or array of numbers.
+
+console.log(Math.random()) // It will return a random number between 0 and 1.
