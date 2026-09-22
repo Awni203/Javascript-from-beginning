a196cbbd8489d9ebdd1eca5668ede7910e635478
Awnish <dubeyawnish203@gmail.com>
2026-09-22 20:04:02 +0000

    Scope is completed in scope and if,else conditioning both

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -1,7 +1,55 @@
-let a = 10;
-const b = 20;
-var c = 30;
+// var c = 300;
 
-console.log(a);
-console.log(b);
-console.log(c);
+let a = 300;
+if (true) { // block scope :- it is a block scope because it is written inside the {curly braces}.
+    let a = 10;
+    const b = 20;
+    console.log("INNER:",a);// output will be INNER: 10
+}
+
+// console.log(a);// output will be 300 because we are calling a global scope 
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "Awnish"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5) // cannot executed, gives a error because we hold the function value inside a variable
+const addTwo = function(num){
+    return num + 2
+}
